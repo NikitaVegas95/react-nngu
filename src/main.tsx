fbc060ab9cfcf1ffc2040bcf45ecbad94528bcff
Isaev Nikita <nikitavegas95@gmail.com>
2025-01-12 18:36:31 +0300

    feat: store redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.scss'
 import {HomePage} from "./pages";
+import store from "./store/store.ts";
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HomePage />
+      <Provider store={store}>
+          <HomePage />
+      </Provider>
   </StrictMode>,
 )
--- src/pages/ui/HomePage.tsx
+++ src/pages/ui/HomePage.tsx
@@ -1,28 +1,17 @@
 import {Container} from "../../components/User";
-import { useEffect, useState } from "react";
+import {useEffect} from "react";
 import {UserMaps} from "../../components/User/maps";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchUserData} from "../../store/user/thunk.ts";
+import {getUser} from "../../store/user/selector.ts";
 
 export const HomePage = () => {
-    const [data, setData] = useState()
-
-    const user: string = 'c8b5635a-90f1-4d8e-b5a9-a77a79fa4c98'
-
-    const baseUrl = (params: string) => {
-        return `https://mocki.io/v1/${params}`;
-    }
-
-    const getAllUsers = async (params: string) => {
-        const getData = await fetch(baseUrl(params));
-        return await getData.json();
-    }
+    const dispatch = useDispatch();
+    const { data } = useSelector(getUser);
 
     useEffect(() => {
-        getAllUsers(user).then(item => setData(item))
-    }, []);
-
-    if (!data) return (
-        <div>Данных нет</div>
-    );
+        dispatch(fetchUserData() as any)
+    }, [dispatch]);
 
     return (
         <Container>
